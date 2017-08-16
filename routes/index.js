b622f063fade090a2870347b0cc5456b72cf6f96
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-16 13:19:25 -0700

    fixing some routes

--- routes/index.js
+++ routes/index.js
@@ -12,12 +12,38 @@ router.post('/createBet', function(req, res) {
     new Bets({
         wager: req.body.wager,
         content: req.body.content,
+        bettor: req.body.bettor,
+        bettee: req.body.bettee,
         pending: true,
         alreadyWon: false,
     }).save((err, savedBet) => {
         console.log('Bet saved: ', savedBet);
         res.json(savedBet);
     })
+});
+
+router.post('/feed', function(req, res) {
+    Bets.find((err, allBets) => {
+        res.json(allBets);
+    });
+});
+
+router.post('/myBets', function(req, res) {
+    console.log(req.body.bettee);
+    Bets.find({ $or: [{ bettor: req.body.bettor }, { bettee: req.body.bettee }]}, (err, myBets) => {
+        res.json(myBets);
+    });
+});
+
+router.post('/viewOneBet/:id', function(req, res) {
+
+});
+
+
+
+
+router.post('/myPendingBets', function(req, res) {
+
 });
 
 module.exports = router;
