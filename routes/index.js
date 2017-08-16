ee023e3c0630cc1e54e77db6915a196ccd80bc38
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-16 16:14:00 -0700

    yay done with routes for now

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var Users = models.User;
 router.get('/', function(req, res) {
     res.send('HEY BABES');
 });
+
 router.post('/register', function(req, res){
   new User({
     facebookId: req.body.id,
@@ -19,13 +20,14 @@ router.post('/register', function(req, res){
     res.json(savedUser);
   })
 })
+
 router.post('/createBet', function(req, res) {
     new Bets({
         wager: req.body.wager,
         content: req.body.content,
         bettor: req.body.bettor,
         bettee: req.body.bettee,
-        pending: true,
+        pending: false,
         alreadyWon: false,
     }).save((err, savedBet) => {
         console.log('Bet saved: ', savedBet);
@@ -35,6 +37,7 @@ router.post('/createBet', function(req, res) {
 
 router.post('/feed', function(req, res) {
     Bets.find((err, allBets) => {
+        console.log('Found all them bets!');
         res.json(allBets);
     });
 });
@@ -42,19 +45,36 @@ router.post('/feed', function(req, res) {
 router.post('/myBets', function(req, res) {
     console.log(req.body.bettee);
     Bets.find({ $or: [{ bettor: req.body.bettor }, { bettee: req.body.bettee }]}, (err, myBets) => {
-        res.json(myBets);
+        if (err) {
+            console.log('Oh my my an error: ', err);
+        } else {
+            console.log('All my bets in the haus: ', myBets);
+            res.json(myBets);
+        }
     });
 });
 
 router.post('/viewOneBet/:id', function(req, res) {
-
+    console.log(req.params.id);
+    Bets.findById(req.params.id, (err, bet) => {
+        if (err) {
+            console.log('Oh no an error: ', err);
+        } else {
+            console.log('That one bet was found hoorah: ', bet);
+            res.json(bet);
+        }
+    });
 });
 
-
-
-
 router.post('/myPendingBets', function(req, res) {
-
+    Bets.find({ pending: true }, (err, myPendingBets) => {
+        if (err) {
+            console.log('Oh my my an error: ', err);
+        } else {
+            console.log('Pending bets woot woot: ', myPendingBets);
+            res.json(myPendingBets);
+        }
+    });
 });
 
 module.exports = router;
